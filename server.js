ceae18e181bc76be1f98359a4c34aa6306567e20
Mulho MABIALA <imulhomabiala@gmail.com>
2026-02-17 23:11:06 +0000

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*',
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
